e28b906081bdbf3a556b1671b1de328fbeb92917
Tarmo Petrovits <tarmo@arego.ee>
2022-02-09 16:37:13 +0200

    Add MSW for server mocking

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,12 @@ import ReactDOM from 'react-dom';
 
 import { App } from './App';
 
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line global-require
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <CssBaseline />
---
+++ src/mocks/handlers.js
@@ -0,0 +1,54 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { rest } from 'msw';
+
+export const handlers = [
+  // eslint-disable-next-line consistent-return
+  rest.post('/login', (req, res, ctx) => {
+    const { username, password } = req.body;
+
+    if (username === 'test@test.ee' && password === 'password1234') {
+      sessionStorage.setItem('is-authenticated', 'true');
+      const response = {
+        message: {
+          success: true,
+          description: '',
+          data: [],
+          type: ''
+        }
+      };
+      return res(ctx.delay(300), ctx.status(200), ctx.json({ ...response }));
+    }
+    if (username !== 'test@test') {
+      const response = {
+        message: {
+          success: false,
+          description: '',
+          data: [],
+          type: 'error'
+        },
+        error: {
+          code: 123,
+          description: 'Something went wrong'
+        }
+      };
+      return res(ctx.delay(300), ctx.status(200), ctx.json({ ...response }));
+    }
+  }),
+  rest.get('/user', (req, res, ctx) => {
+    const isAuthenticated = sessionStorage.getItem('is-authenticated');
+    if (!isAuthenticated) {
+      return res(
+        ctx.status(403),
+        ctx.json({
+          errorMessage: 'Not authorized'
+        })
+      );
+    }
+    return res(
+      ctx.status(200),
+      ctx.json({
+        username: 'admin'
+      })
+    );
+  })
+];
